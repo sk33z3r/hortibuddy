5d5a32c243ffc4a14d89d175d75aa333cf76c27f
sk33z3r <sk33z3r@protonmail.com>
2018-07-10 21:28:12 -0400

    Add Create DB button, styled clicks a little more for all buttons

--- index.php
+++ index.php
@@ -18,7 +18,7 @@ $files = scandir("db/", 0); ?><!DOCTYPE html>
             <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="#">upgrade your browser</a> to improve your experience.</p>
         <![endif]-->
         <div id="main">
-            <h2>Available Databases</h2>
+            <h2>Open or Create a Database</h2>
             <?php
                 // Grab the filenames of all .hbd files in the db folder, then strip out .hbd and present the user with all the options
                 // Start with 2 because unix prints `.` and `..` as items in the array
@@ -26,6 +26,7 @@ $files = scandir("db/", 0); ?><!DOCTYPE html>
                     print '<a href="php/menu.php?user='.substr($files[$i], 0, -4).'"><button>'.substr($files[$i], 0, -4).'</button></a>';
                 }
             ?>
+            <a href="php/new-db.php"><button id="create">Create New</button></a>
         </div>
     </body>
 </html>
